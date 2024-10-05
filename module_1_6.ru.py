3eb132815b754dcfe63bab3fbb833cba81fdd46c
Viktoriya-max <viktoriyakoloskova20@gmail.com>
2024-10-05 22:37:50 +0300

    Add files via upload

---
+++ module_1_6.ru.py
@@ -0,0 +1,23 @@
+#work with dictionaries
+my_dict={'Max':2024, 'Oleg':1976, 'Irina':1987 }
+print(my_dict)
+print(my_dict['Max'])
+my_dict['Kira']=2020
+print(my_dict['Kira'])
+my_dict.update({'Nina':2010, 'Pitr':2001 })
+print(my_dict)
+print(my_dict.pop('Irina'))
+print(my_dict)
+#work with sets
+my_set={1, 11, 1, 'one', 2.0, False, 2.0, 'one'}
+print(my_set)
+my_set.update({21,21,'yar'})
+print(my_set)
+print(my_set.discard(2.0))
+print(my_set)
+
+
+
+
+
+
